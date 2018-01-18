71af1434820d3f83aca90f2d98697702c703a696
Bibata Rabba Idi <bibatarabbaidi@yahoo.com>
2018-01-17 23:26:17 -0500

    Initial commit

---
+++ Introduction.java
@@ -0,0 +1,16 @@
+
+/**
+ * Program: Introduction File:Introduction.java Project: MyFirstProject Summary:
+ * Prints user's information to console Author: Bibata Rabba Idi Date:01/14/2018
+ *
+ */
+
+public class Introduction {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Bibata");
+        System.out.println("My favorite color is white");
+        System.out.println("If I won the lottery, I would donate to poor people");
+    }
+    
+}
